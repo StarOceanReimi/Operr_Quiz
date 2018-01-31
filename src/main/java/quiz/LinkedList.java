35c7021f985f90c4ded7bb6b958cee25b5a61b3c
Li Qiu <saiqiuli@gmail.com>
2018-01-30 19:19:48 -0500

    todo unit test

--- src/main/java/quiz/LinkedList.java
+++ src/main/java/quiz/LinkedList.java
@@ -1,5 +1,133 @@
 package quiz;
 
-public class LinkedList {
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+/**
+ * Two-way linked list
+ * @author Li Qiu
+ */
+public class LinkedList<T extends Comparable<T>> implements Iterable<T> {
+
+    private static class Node<T> {
+        Node(T value, Node<T> prev, Node<T> next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+        Node<T> next;
+        Node<T> prev;
+        T value;
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+
+    private int size;
+
+    public LinkedList() {
+        size = 0;
+        head = null;
+        tail = null;
+    }
+
+    private boolean emptyList() {
+        return head == null && tail == null && size == 0;
+    }
+
+    private void resetPointerToNull() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    private Node<T> removeNode(Node<T> node) {
+        Objects.requireNonNull(node, "node can not be null");
+        Node<T> prev = node.prev;
+        Node<T> next = node.next;
+        if(prev != null) prev.next = next;
+        if(next != null) next.prev = prev;
+        size--;
+        //last element
+        if(size == 0) resetPointerToNull();
+        return next;
+    }
+
+    /**
+     * Append an element into list
+     */
+    public void push(T value) {
+        if(emptyList()) {
+            head = new Node<T>(value, null, null);
+            tail = head;
+        } else {
+            tail.next = new Node<T>(value, tail, null);
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    /**
+     * 
+     */
+    public T pop() {
+        if(emptyList()) return null;
+        size--;
+        Node<T> poped = tail;
+        tail = poped.prev;
+        //if tail is not first element
+        if(tail != null) tail.next = null;
+        else resetPointerToNull();
+        return poped.value;
+
+    }
+
+    public void removeAllGeater(T value) {
+        removeIf(t->t.compareTo(value) > 0);
+    }
+
+    /**
+     * go through list remove node with condition
+     */
+    public void removeIf(Predicate<T> pred) {
+        Node<T> cur = head;
+        while(cur != null) {
+            if(pred.test(cur.value))
+                cur = removeNode(cur);
+            else
+                cur = cur.next;
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator(this.head);
+    }
+
+    private class ListIterator implements Iterator<T> {
+
+        Node<T> cur;
+        ListIterator(Node<T> cur) {
+            this.cur = cur;
+        }
+
+        @Override
+        public void remove() {
+            cur = LinkedList.this.removeNode(cur.prev);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cur != null;
+        }
+
+        @Override
+        public T next() {
+            T val = cur.value;
+            cur = cur.next;
+            return val;
+        }
+
+    }
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,8 +5,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+
+
+
+    @Test
+    public void testPush() {
+
+    }
+
+    @Test
+    public void testPop() {
+
+    }
+
+    @Test
+    public void testRemoveAllGreater() {
+
     }
 }
